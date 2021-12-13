33a004ca679f9ce211e63b9c030a324419ed0113
Danil <danilket@mail.ru>
2021-12-13 16:35:22 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <users></users>
+  </div>
+</template>
+
+<script>
+import Users from "./components/Users.vue";
+
+export default {
+  name: "App",
+  components: {
+    Users,
+  },
+};
+</script>
---
+++ src/components/Users.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul class="users">
+      <li class="user" v-for="user in users" :key="user.id">
+        <h4 class="user__title">{{ user.name }}</h4>
+        <a class="user__email" :href="`mailto:${user.email}`">
+          {{ user.email }}</a
+        >
+        <a class="user__phone" :href="`tel:${user.phone.replace(/[^\d]/g, '')}`"
+          >{{ user.phone }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: "",
+    };
+  },
+  methods: {
+    async signIn() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+      const data = await res.json();
+
+      this.users = data;
+    },
+  },
+  mounted() {
+    this.signIn();
+  },
+};
+</script>
